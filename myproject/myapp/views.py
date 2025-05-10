ab29fb03ea082e789409ca7b232b95d4eeab4b4f
Владимир Мережко <Merezhko11@rambler.ru>
2025-05-10 21:37:48 +0300

    add files1

---
+++ myproject/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
+
+def contact(request):
+    return render(request, 'myapp/contact.html')
+
+def services(request):
+    return render(request, 'myapp/services.html')
